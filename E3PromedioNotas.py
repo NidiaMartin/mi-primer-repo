25d1694abe8eafcd3bc6396c74e3ee07d58567ea
NAMB <gestionandreamartin@gmail.com>
2026-08-08 01:02:51 -0500

    archivo python promedio notas

---
+++ E3PromedioNotas.py
@@ -0,0 +1,15 @@
+# Solicitar las notas del estudiante
+print("Por favor, ingrese la nota de la actividad 1: ")
+nota1 = float(input())
+
+print("Por favor, ingrese la nota de la actividad 2: ")
+nota2 = float(input())
+
+print("Por favor, ingrese la nota de la actividad 3: ")
+nota3 = float(input())
+
+# Calcular el promedio
+promedio = (nota1 + nota2 + nota3) / 3
+
+# Mostrar el resultado
+print("El promedio del estudiante es:", promedio)
